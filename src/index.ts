f491491da84e0c3daa48a77b8229851153c330d7
Alejandro Marvich <alejandromarvich181100@gmail.com>
2025-01-16 17:41:08 -0300

    multer para cargar imagenes desde fdm admin

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import productsRouter from './router/products'
 import { corsMiddleware } from './services/cors'
 import path from 'path'
 import dotenv from 'dotenv'
+import multer from 'multer'
 
 dotenv.config()
 
@@ -12,10 +13,33 @@ app.use(corsMiddleware())
 app.use(express.json())
 app.use('/images', express.static(path.resolve('public/images')))
 
+const storage = multer.diskStorage({
+  destination: (_req: express.Request, _file: Express.Multer.File, cb: (error: Error | null, destination: string) => void) => {
+    cb(null, path.join(__dirname, '../public/images/products/fondo transparente'))
+  },
+  filename: (_req: express.Request, file: Express.Multer.File, cb: (error: Error | null, filename: string) => void) => {
+    cb(null, `${file.originalname}`)
+  }
+})
+
+const upload = multer({ storage })
+
 const PORT = (process.env.PORT !== null && process.env.PORT !== undefined) ? process.env.PORT : 1234
 
 app.get('/', (_req, res) => {
   res.redirect('/products')
+})
+app.post('/upload', upload.array('images', 10), (req, res) => {
+  try {
+    const files = req.files as Express.Multer.File[]
+    if (!files) {
+      throw new Error('No files found')
+    }
+    const filePaths = files.map(file => `images/products/fondo transparente/${file.filename}`)
+    res.status(200).json({ message: 'images uploaded', files: filePaths })
+  } catch (error) {
+    res.status(400).json({ message: 'Error al cargar las imagenes', error })
+  }
 })
 
 app.use('/products', productsRouter)
